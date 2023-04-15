a8b543ab8028f6387e889f0a0d86efcbd8903639
Angel Diaz <warydiaz@hotmail.com>
2023-04-15 18:36:15 +0200

    We're adding a new class to store all of our constants, called Constants. Then, we'll use those constants in our code.

---
+++ Constant/Api.ts
@@ -0,0 +1,4 @@
+// apiUrls.ts
+
+export const USER_API_URL = 'https://randomuser.me/api?results=';
+export const PRODUCT_API_URL = 'https://example.com/api/products';
--- Services/UserServicesImp.ts
+++ Services/UserServicesImp.ts
@@ -1,11 +1,12 @@
 import { UserServices } from "./UserServices.js";
 import { User } from "../user.js";
+import { USER_API_URL} from "../Constant/Api.js"
 
 export class UserServicesImp implements UserServices {
   constructor() {}
 
   public loadUsers = async (n: number) => {
-    const response = await fetch(`https://randomuser.me/api?results=${n}`);
+    const response = await fetch(`${USER_API_URL}${n}`);
     const { results } = (await response.json()) as { results: any[] };
     const users: Array<User> = [];
     for (const { gender, name, location, login, email, picture } of results) {
