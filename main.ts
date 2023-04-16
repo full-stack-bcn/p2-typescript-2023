0e459b424f5d2a0fa0de125b07d571a0873ef8bf
Angel Diaz <warydiaz@hotmail.com>
2023-04-16 13:33:46 +0200

    Adding new interface to the main.

--- main.ts
+++ main.ts
@@ -2,10 +2,13 @@ import { writeFile } from "fs/promises";
 import { render } from "./render.ts";
 import { UserServices } from "./Services/UserServices.js";
 import { UserServicesImp } from "./Services/UserServicesImp.js";
+import { RenderServices } from "./Services/RenderServices.js";
+import { RenderServicesImp } from "./Services/RenderServicesImp.js";
 
 
 
 const userService: UserServices = new UserServicesImp();
 const users = await userService.loadUsers(100);
-const html = render(users);
+const renderServices: RenderServices = new RenderServicesImp();
+const html = await renderServices.render(users);
 await writeFile('users.html', html);
